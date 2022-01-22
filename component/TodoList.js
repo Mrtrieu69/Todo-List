b2cdcad08a159f84a3c22484cd7ea984f87b31f8
Mrtrieu69 <trieutam2808@gmail.com>
2022-01-22 12:00:51 +0300

    ok

---
+++ component/TodoList.js
@@ -0,0 +1,27 @@
+import html from "../core.js"
+import { connect } from "../store.js"
+import TodoItem from "./TodoItem.js"
+
+function TodoList({ todos, filter, filters, editIndex }){
+    return html`
+        <section class="main">
+            <input 
+            id="toggle-all" 
+            class="toggle-all" 
+            type="checkbox"
+            onchange="dispatch('toggleAll', this.checked)"   
+            ${todos.every(filters.completed) && "checked"} 
+        >
+            <label for="toggle-all">Mark all as complete</label>
+            <ul class="todo-list">
+                ${todos.map((todo, index) =>{
+                    if(todos.filter(filters[filter]).includes(todo)){
+                        return TodoItem(todo, index, editIndex)
+                    }
+                })}
+            </ul>
+        </section>
+    `
+}
+
+export default connect()(TodoList)
